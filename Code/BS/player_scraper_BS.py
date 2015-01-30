13b0af2d54ebf5020c33f30149644c28cd9ebb1f
emeryong <emery.ong@gmail.com>
2015-01-30 11:06:04 +0800

    Update player_scraper_BS.py

--- Code/BS/player_scraper_BS.py
+++ Code/BS/player_scraper_BS.py
@@ -1,1 +1,16 @@
 # to scrap players infos using BS4
+# written using Python 3.x
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+# declare URL to scrape from and store html in content variable
+thisURL = 'http://www.basketball-reference.com/players/c/cabocbr01.html'
+content = urllib.request.urlopen(thisURL).read()
+
+# process html using BeautifulSoup
+soup = BeautifulSoup(content)
+
+# work in progress - Emery
+playerName = soup.find('h1').string
+x = soup.find_all('p','padding_bottom_half')
