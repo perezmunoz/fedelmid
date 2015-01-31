dfe0695d34edb046e2cabb8da89b8f3e867bceb7
Simodezenne <helmlin@comp.nus.edu.sg>
2015-01-31 11:06:36 +0800

    beginning of team_scraper

--- Code/BS/team_scraper_BS.py
+++ Code/BS/team_scraper_BS.py
@@ -1,1 +1,18 @@
 # to scrap team infos using BS4
+
+from bs4 import BeautifulSoup
+import re
+import urllib
+import string
+
+# Get the location of a team given its url
+
+def team_location(team_url):
+	thisURL = "http://basketball-reference.com"+team_url
+	soup = BeautifulSoup(urllib.urlopen(thisURL).read())
+	tag = soup.find("div",class_="mobile_text")
+	print(tag)
+
+	# Work in progress - Simon
+
+team_location("/teams/ATL/")
