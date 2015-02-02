6d4fb8fd7a4644385a12509dd2125fee6c55aa50
ThiDiff <juanmanuel.munozperez@gmail.com>
2015-02-02 10:34:50 +0800

    Scraping code for profile, statistic's players which could be reuse for team's statistics

--- Code/BS/links_provider_BS.py
+++ Code/BS/links_provider_BS.py
@@ -62,3 +62,80 @@ def number_all_players():
 		if c!='x':		#There is no player whose name starts with x
 			res+=number_players(c)
 	return res
+
+=============================================================================================
+"Juan's code"
+=============================================================================================
+
+from bs4 import BeautifulSoup
+import urllib # Library used to open links
+import re
+import time
+
+# URL start point url = http://www.basketball-reference.com
+# For name's players starting with a, the URL is ".../players/a"
+
+# Get player's first letter in name
+def get_letters_players():
+	# Links are stored in an unique <table>...</table> HTML tag
+	links = []
+
+	# Opening index.html file which corresponds to url
+	index = BeautifulSoup(urllib.urlopen('http://www.basketball-reference.com/players/').read())
+
+	for row in index('td', {'class': 'align_center bold_text valign_bottom xx_large_text'}):
+		links.append(str(row.a.get('href').split('/')[2]))
+
+	# links represent the list of alphabetical players names
+	# Ex: links[0] = /players/a/
+	return links
+
+# Get player's links and names for a specific letter
+def get_players_by_letter(letter):
+
+	# List containing player's links
+	links_players = []
+
+	# List containing player's names
+	names_players = []
+
+	# Index of players whose name start by 'letter'
+	index = BeautifulSoup(urllib.urlopen('http://www.basketball-reference.com/players/'+letter).read())
+	
+	# Retrieve url links or names
+	for row in index('a', href=re.compile('/players/'+letter)):
+		# Sleeps 1 second
+		# time.sleep(1)
+		links_players.append(str(row.get('href').split('/')[3]))
+		names_players.append(str(row.get_text()))
+	
+	return links_players, names_players
+
+# Note: result can be stored in res = [[]] (list of list)
+# Then res[0] = links_players and res[1] = names_players
+# For each, elements are accessed by res[0][50] = links_players[50]
+
+# Get all links and names of the players
+def get_all_players():
+
+    alphabetical = get_letters_players()
+
+    # Lists containing links and name's players
+    all_links_players = []
+    all_names_players = []
+
+    for letter in alphabetical:
+    	all_players = get_players_by_letter(letter)
+    	all_links_players += all_players[0]
+    	all_names_players += all_players[1]
+
+    return all_links_players, all_names_players
+
+# Launching the code
+# List of list containing links and name's players
+# res = [[]]
+# res = get_all_players()
+
+# res[0] contains links and res[1] contains name's players
+
+
--- Code/BS/player_scraper_BS.py
+++ Code/BS/player_scraper_BS.py
@@ -26,3 +26,218 @@ for info1 in infoall:
 # get stats totals
 
 # get stats per game
+
+=============================================================================================
+"Juan's code"
+=============================================================================================
+
+from bs4 import BeautifulSoup
+import urllib # Library used to open links
+import re
+import time
+import csv
+
+# Prior to launch this script, source scraping_links_players.py (execfile command)
+
+def get_profile_player(link_player):
+
+	# List containing the player's profile
+	res = []
+
+	# Letters of the players
+	# execfile('scraping_links_players.py') prior to lunch this script
+	alphabetical = get_letters_players()
+
+	index = BeautifulSoup(urllib.urlopen('http://www.basketball-reference.com/players/'+link_player[0]+'/'+link_player).read())
+	# class=padding_bottom_half is unique
+	# The used of find_all returns a bs4.element.ResultSet element which cannot be accessed
+	# find method returns a Tag object which can be accessed with common methods
+	profile = index.find('p', {'class': 'padding_bottom_half'})
+
+	# Retrieve all text data from profile (outside tags)
+	# profile_str = profile.get_text()
+	# Removing \xa0 and \u25aa characters (using unicode characters)
+	# profile_str.replace(u'\xa0\u25aa\xa0',u' ').replace(u'\n',u'')
+
+	# A way to chain these two previous lines
+	profile_str = str((profile.get_text()).replace(u'\xa0\u25aa\xa0',u' ').replace(u'\n',u''))
+
+	# profile_str.split('Position:','Shoots:','Height:','Weight:','Born:','High School:','College:','Draft:','NBA Debut:','Other Pages:')
+	# re.split('[Position: Shoots: Height:]',profile_str)
+	profile_split = re.split('(?:Position:|Shoots:|Height:|Weight:|Born:|High School:|College:|Draft:|NBA Debut:|Other Pages:)',profile_str)
+
+	# Remove 1st element which is empty
+	del profile_split[0]
+
+	# Strip each element of the list. 
+	res += map(str.strip,profile_split)
+	# Player's profile:
+	# [0] Position
+	# [1] Shoots
+	# [2] Heigh
+	# [3] Weight
+	# [4] Born (date and place)
+	# [5] High School
+	# [6] College
+	# [7] Draft
+	# [8] NBA Debut
+	# [9] Others Pages
+	return res
+
+def get_all_profile_player():
+
+	# Player's profile list
+	profiles = [[]]
+
+	for player in res[0]:
+		profiles += get_profile_player(player)
+		# Output to algorithm's steps
+		print player
+
+	return profiles
+
+# Get all profile's characteristics in order to build player's profile
+def get_all_characteristics():
+
+	# Date regex
+	date_regex = re.compile("[A-Za-z]*\s[0-9]*[,]{1}\s[0-9]*")
+
+	# Characteristics'list
+	properties = []
+
+	# res[0] contains all url's players
+	res = [[]]
+	res = get_all_players()
+
+	for player in res[0]:
+
+		# Sleeps 1 seconde
+		time.sleep(1)
+
+		# Player's soup
+		index = BeautifulSoup(urllib.urlopen('http://www.basketball-reference.com/players/'+player[0]+'/'+player).read())
+		# Retrieving p tag containing relevant information
+		profile = index.find('p', {'class': 'padding_bottom_half'})
+		# Get all span tags containing characteristics
+		span_tag = profile.find_all('span')
+
+		# Testing whether it's already in properties list or not
+		for char in span_tag:
+			char_str = str(char.get_text())
+			if date_regex.match(char_str) or char_str=='':
+				print "Date or blank avoided"
+				print char_str
+			else:
+				if char_str in properties:
+					print "Characteristic already in properties list"
+				else:
+					properties.append(char_str)
+	
+	return properties
+
+def get_statistics_player(link_player):
+	# Verified with smithis player
+	index = BeautifulSoup(urllib.urlopen('http://www.basketball-reference.com/players/'+link_player[0]+'/'+link_player+'.html').read())
+	# Sleeps 1 second
+	time.sleep(1)
+
+	# Get all tables
+	tables = index.find_all('table')
+	# Avoid computing 'Links' table which is useless
+	for t in tables[0:len(tables)-1]:
+			with open(str(t.get('id'))+'.txt', 'w') as output:
+				# Writing header to the file
+				# We take the last line in header (some tables have mutiple header lines)
+				print str(t.get('id'))
+
+				# Tables which don't have thead tag
+				if t.thead is not None:
+					header = t.thead.find_all('tr')[-1].find_all('th')
+				else:
+					# No thead tag means that 1st row in the tbody tag is the header
+					header = t.find_all('tr')[0].find_all('th')
+
+				for i in range(len(header)):
+					# Last element should be erased when importing data
+					output.write(str(header[i].get_text())+',')
+				# Back to line
+				# Add the player_id (equivalent to html link) at the end
+				output.write('Player_id'+'\n')
+				
+				# Tables which don't heave tbody tag
+				if t.body is not None:
+					body = t.tbody.find_all('tr')
+				else:
+					body = t.find_all('tr')[1:]	
+
+				# Writing data to the file
+				for row in body:
+					data = row.find_all('td')
+					for j in range(len(data)):
+						output.write(str(data[j].get_text())+',')
+					output.write(link_player+'\n')
+
+
+	# Get tables
+#	totals = index.find('table', {'id': 'totals'})
+#	per_game = index.find('table', {'id': 'per_game'})
+#	per_minute = index.find('table', {'id': 'per_minute'})
+#	per_poss = index.find('table', {'id': 'per_poss'})
+#	advanced = index.find('table', {'id': 'advanced'})
+#	shooting = index.find('table', {'id': 'shooting'})
+#	advanced_pbp = index.find('table', {'id': 'advanced_pbp'})
+#	playoffs_totals = index.find('table', {'id': 'playoffs_totals'})
+#	playoffs_per_game = index.find('table', {'id': 'playoffs_per_game'})
+#	playoffs_per_minute = index.find('table', {'id': 'playoffs_per_minute'})
+#	playoffs_per_poss = index.find('table', {'id': 'playoffs_per_poss'})
+#	playoffs_advanced = index.find('table', {'id': 'playoffs_advanced'})
+#	playoffs_shooting = index.find('table', {'id': 'playoffs_shooting'})
+#	playoffs_advanced_pbp = index.find('table', {'id': 'playoffs_advanced_pbp'})
+#	sim_thru = index.find('table', {'id': 'sim_thru'})
+#	sim_career = index.find('table', {'id': 'sim_career'})
+#	college = index.find('table', {'id': 'college'})
+#	salaries = index.find('table', {'id': 'salaries'})
+#	contract = index.find('table', {'id': 'contract'})
+
+	# Opening file to write in the data contained in the tables
+#	with open('smithis01.txt', 'w') as output:
+		
+		# Writing header to the file
+#		header_totals = totals.thead.tr.find_all('th')
+#		for i in range(len(header_totals)):
+			# Last element should be erased when importing data
+#			output.write(str(header_totals[i].get_text())+',')
+		# Back to line
+		# I add the link to the data in order to find to whom belongs it
+#		output.write('Player_id'+'\n')
+		
+		# Writing header to the file
+#		body_totals = totals.tbody.find_all('tr')
+#		for row in body_totals:
+#			data = row.find_all('td')
+#			for j in range(len(data)):
+#				output.write(str(data[j].get_text())+',')
+#			output.write('smithis01'+'\n')
+
+
+# Tables index
+# Totals id='totals'
+# Per Game id='per_game'
+# Per 36 Minutes id='per_minute'
+# Advanced id='advanced'
+# Per 100 Poss id='per_poss'
+# Shooting id='shooting'
+# Play-by-Play id='advanced_pbp'
+# Contract id='stats_table'
+# Playoffs Totals id='playoffs_totals'
+# Playoffs Per Game id='playoffs_per_game'
+# Playoffs Per Minute id='playoffs_per_minute'
+# Playoffs Per 100 Poss id='playoffs_per_poss'
+# Playoffs Advanced id='playoffs_advanced'
+# Playoffs Shooting id='playoffs_shooting'
+# Playoffs Play-by-Play id='playoffs_advanced_pbp'
+# Salaries id='salaries'
+
+# Test if list is empty
+# if not element:
+#	print "element is empty"
