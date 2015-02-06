e0c2cfe9711c3b519765e027b3d22fbd1f7f0b48
ThiDiff <juanmanuel.munozperez@gmail.com>
2015-02-06 20:58:59 +0800

    Writing the report

---
+++ Code/SQLite/question_2.sql
@@ -0,0 +1,31 @@
+# How many active players there are during the 2011-2012 season?
+SELECT COUNT(*)
+	FROM PROFILEF A, PLAYER_TOTAL B
+	WHERE PLAYERID IN (SELECT PLAYERID FROM ACTIVEPLAYERS WHERE ACTIVE = "TRUE")
+		AND A.PLAYERID = B.PLAYERID
+		AND B.SEASON = 2011;
+
+# How many play in each position?
+SELECT POSITION, COUNT(*)
+	FROM PROFILEF A, PLAYER_TOTAL B
+	WHERE PLAYERID IN (SELECT PLAYERID FROM ACTIVEPLAYERS WHERE ACTIVE = "TRUE")
+		AND A.PLAYERID = B.PLAYERID
+		AND B.SEASON = 2011
+	GROUP BY POSITION;
+
+# What is the average age, average weight, average experience, average salary in the season, average career salary?
+WITH s1 AS
+	(SELECT SUM(salary) as c_sal, playerid
+	 FROM salary
+ 	 WHERE season < 2014 AND playerid IN (SELECT playerid FROM activeplayers WHERE active = "TRUE")
+ 	 GROUP BY playerid)
+SELECT avg(date('now')-a.dob)
+      ,avg(a.weight)
+      ,avg(a.experience)
+      ,avg(b.salary)
+      ,avg(c.c_sal)
+	FROM profilef a, salary b, s1 c
+	WHERE a.playerid IN (SELECT playerid FROM activeplayers WHERE active = "TRUE")
+		AND a.playerid = b.playerid
+		AND a.playerid = c.playerid
+		AND b.season = 2011;
---
+++ Code/SQLite/question_4.sql
@@ -0,0 +1,10 @@
+# What is the average salary of each team by season of each team starting in 2002 and finishing 2012 season?
+# What is the variance of the salaries?
+
+
+# What is the average age of the players by season?
+# Average and variance of experience by season of each team?
+
+
+# Can you provide the above in a "cross tabulation" format?
+# That is, teams are on each row, each column is a year, and the values are the metrics above?
