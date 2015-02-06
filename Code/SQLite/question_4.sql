39957d0bc7b09cd55fc671c6a61161777c79172b
emeryong <emery.ong@gmail.com>
2015-02-07 02:12:42 +0800

    Update question_4.sql

--- Code/SQLite/question_4.sql
+++ Code/SQLite/question_4.sql
@@ -1,8 +1,20 @@
 # What is the average salary of each team by season of each team starting in 2002 and finishing 2012 season?
+SELECT TEAM, SEASON, AVG(SALARY)
+FROM SALARY WHERE SEASON BETWEEN 2002 AND 2011
+GROUP BY TEAM, SEASON;
+
 # What is the variance of the salaries?
 
 
 # What is the average age of the players by season?
+SELECT B.SEASON, B.TEAM, AVG(DATE('now')-A.DOB)
+FROM PROFILEF A, PLAYER_TOTAL B
+WHERE A.PLAYERID = B.PLAYERID
+  AND A.PLAYERID IN (SELECT PLAYERID FROM ACTIVEPLAYERS WHERE ACTIVE = "TRUE")
+  AND B.TEAM <> ""
+  AND B.SEASON BETWEEN 2002 AND 2011
+GROUP BY SEASON, TEAM;
+
 # Average and variance of experience by season of each team?
 
 
