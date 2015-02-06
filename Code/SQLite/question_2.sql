13cd43a5bd05a6c37b4a1c8e5aa2b940539c7482
emeryong <emery.ong@gmail.com>
2015-02-06 21:40:14 +0800

    Update question_2.sql

--- Code/SQLite/question_2.sql
+++ Code/SQLite/question_2.sql
@@ -6,12 +6,15 @@ SELECT COUNT(*)
 		AND B.SEASON = 2011;
 
 # How many play in each position?
-SELECT POSITION, COUNT(*)
-	FROM PROFILEF A, PLAYER_TOTAL B
-	WHERE PLAYERID IN (SELECT PLAYERID FROM ACTIVEPLAYERS WHERE ACTIVE = "TRUE")
-		AND A.PLAYERID = B.PLAYERID
-		AND B.SEASON = 2011
-	GROUP BY POSITION;
+	 SELECT X.POSITION, COUNT(*)
+	 FROM (SELECT DISTINCT PLAYERID, POSITION 
+               FROM PLAYER_TOTAL 
+               WHERE PLAYERID IN (SELECT A.PLAYERID 
+                                  FROM PROFILEF A, PLAYER_TOTAL B
+	                          WHERE PLAYERID IN (SELECT PLAYERID FROM ACTIVEPLAYERS WHERE ACTIVE = "TRUE")
+	                          AND A.PLAYERID = B.PLAYERID
+	                          AND B.SEASON = 2011) X
+	 GROUP BY X.POSITION;
 
 # What is the average age, average weight, average experience, average salary in the season, average career salary?
 WITH s1 AS
