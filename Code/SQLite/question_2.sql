3c4d791f47d9449469fc65a82cf07fead1317eee
emeryong <emery.ong@gmail.com>
2015-02-08 09:06:03 +0800

    Update question_2.sql

--- Code/SQLite/question_2.sql
+++ Code/SQLite/question_2.sql
@@ -1,5 +1,5 @@
 # How many active players there are during the 2011-2012 season?
-SELECT COUNT(*)
+SELECT COUNT(*) AS "ACTIVE PLAYERS"
 FROM (SELECT DISTINCT A.PLAYERID
 	FROM ACTIVEPLAYERS A, PLAYER_TOTAL B
 	WHERE A.PLAYERID = B.PLAYERID
@@ -8,7 +8,7 @@ FROM (SELECT DISTINCT A.PLAYERID
           AND B.TEAM <> "");
 
 # How many play in each position?
-SELECT POSITION, COUNT(*)
+SELECT POSITION, COUNT(*) AS TOTALS
 FROM (SELECT DISTINCT A.PLAYERID, B.POSITION 
         FROM ACTIVEPLAYERS A, PLAYER_TOTAL B
         WHERE A.ACTIVE = "TRUE"
@@ -25,23 +25,23 @@ WITH s1 AS
 	 FROM salary
  	 WHERE season < 2012 AND playerid IN (SELECT playerid FROM activeplayers WHERE active = "TRUE")
  	 GROUP BY playerid)
-SELECT avg(a.salary)
-      ,avg(b.c_sal)
+SELECT avg(a.salary) as AVG_SALARY
+      ,avg(b.c_sal)  as AVG_CAREER_SALARY
 	FROM salary a, s1 b
 	WHERE a.playerid IN (SELECT playerid FROM activeplayers WHERE active = "TRUE")
 		AND a.playerid = b.playerid
 		AND b.season = 2011;
 
 #average age, weight		
-SELECT avg(b.age)
-      ,avg(a.weight)
+SELECT avg(b.age) as AVG_AGE
+      ,avg(a.weight) as AVG_WEIGHT
 	FROM profilef a, player_total b
 	WHERE a.playerid IN (SELECT playerid FROM activeplayers WHERE active = "TRUE")
 		AND a.playerid = b.playerid
 		AND b.season = 2011;
 
 #average experience
-SELECT AVG(EXPERIENCE)
+SELECT AVG(EXPERIENCE) as AVG_EXPERIENCE
 FROM TEAMROSTER A, ACTIVEPLAYERS B
 WHERE A.PLAYERID = SUBSTR(B.PLAYERID,2)
   AND B.ACTIVE = "TRUE"
